f6d542fe3ef80ef9c2d86c05a17d5039f74f40d5
elsoleiro <elsoleiro@gmail.com>
2020-10-01 23:34:12 +0100

    added shell context processor, which preimports the app instance via flask shell, launches us into the environment

--- mysite.py
+++ mysite.py
@@ -1,2 +1,20 @@
-from app import app
+from app import app, db
+from app.models import User, Card
+
+"""
+flask shell command preimports the application instance, this is the
+configuration.
+the decorator registers the function as a shell context function, when
+the flask shell command runs it will invoke this function and register the
+items returned by it in the shell session.
+in venv:
+    flask shell
+    >> db
+    >> User
+    >> Card
+"""
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Card': Card}
+
 
