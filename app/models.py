edf81c2f3b8a539ca0251eb4d134d6f55dacf037
elsoleiro <elsoleiro@gmail.com>
2020-10-01 23:54:34 +0100

    some login functionality added, requiring users to login is next

--- app/models.py
+++ app/models.py
@@ -1,14 +1,25 @@
 """
-classes inherit from db>model, a base class for all models from flask-sqlalch,
+classes inherit from db.model, a base class for all models from flask-sqlalch,
 this class defines several fields as class variables, fields are created
 as instances of the db.Column class, which takes the field type as an arg,
 plus other optional args
 """
 from datetime import datetime
-from app import db
+from app import db, login
+from flask_login import UserMixin
 
+"""
+the extension (flask-login) expects a configuration as a user loader function
+that can be called to load a user given the id
+the user loader is registered with Flask-Login via the decorator, the id
+that Flask-Login passes to the function as an argument will be a string so
+we convert to int to allow the db to use this
+"""
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
