fe2c7847826cd37f56f7f56e370d54b59f70b5f5
elsoleiro <elsoleiro@gmail.com>
2020-10-02 20:36:53 +0100

    added route for registration

--- app/routes.py
+++ app/routes.py
@@ -45,3 +45,17 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user_is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('registration complete')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
