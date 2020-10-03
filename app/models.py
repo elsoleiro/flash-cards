6309a4baf371d92bb1f96c8d73b1e4a988283cc2
elsoleiro <elsoleiro@gmail.com>
2020-10-03 13:29:43 +0100

    added user avatars for each post aswell within forloop

--- app/models.py
+++ app/models.py
@@ -8,6 +8,7 @@ from datetime import datetime
 from app import db, login
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
+from hashlib import md5
 
 """
 the extension (flask-login) expects a configuration as a user loader function
@@ -35,6 +36,11 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+                digest, size)
 
 class Card(db.Model):
     id = db.Column(db.Integer, primary_key=True)
