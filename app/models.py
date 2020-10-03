a8ef9063e0d888d8ea0530129b019ef876cd52a4
elsoleiro <elsoleiro@gmail.com>
2020-10-03 14:18:39 +0100

    fixed db, migrations brought back in, db is empty

--- app/models.py
+++ app/models.py
@@ -27,6 +27,7 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     cards = db.relationship('Card', backref='author', lazy='dynamic')
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return '<user {}>'.format(self.username)
