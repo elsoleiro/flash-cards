be74eec6d9875bb48ad5ffc535971f8ed93149d3
elsoleiro <elsoleiro@gmail.com>
2020-10-05 18:12:29 +0100

    working on editing card

--- app/routes.py
+++ app/routes.py
@@ -64,6 +64,10 @@ def user(username):
     user = User.query.filter_by(username=username).first_or_404()
 
     return render_template('user.html', user=user)
+@app.route('edit/<card_id>')
+@login_required
+def edit(card_id):
+    return render_template('index.html')
 
 @app.before_request
 def before_request():
