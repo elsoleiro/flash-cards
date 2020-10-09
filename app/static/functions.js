b6c269f3f043e71646793398c7c638ae735fb634
elsoleiro <elsoleiro@gmail.com>
2020-10-09 20:56:22 +0100

    getting closer to resolving this function

---
+++ app/static/functions.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $('.cardBack').hide();
+    if ($('.memorizePanel').length != 0) {
+
+        $('.flipCard').click(function(){
+            if ($('.cardFront').is(":visible") == true) {
+                $('.cardFront').hide();
+                $('.cardBack').show();
+            } else {
+                $('.cardFront').show();
+                $('.cardBack').hide();
+            }
+        });
+    }
+});
+
