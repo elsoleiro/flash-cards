d60e2be44ec1d860749f4ad3b7ed5d4fe6a6a0a7
elsoleiro <elsoleiro@gmail.com>
2020-10-11 12:26:45 +0100

    further testing, make input functionality

--- app/static/app.js
+++ app/static/app.js
@@ -1,6 +1,15 @@
+function getVar() {
+    document.getElementById("var").innerHTML = max;
+    
+};
+
+
 var clicks = 0;
 function counter() {
-        clicks += 1;
-        document.getElementById("clicks").innerHTML = clicks;
+    clicks += 1;
+    if (clicks == 10) {
+        clicks = 0
+    };
+    document.getElementById("clicks").innerHTML = clicks;
 };
 
