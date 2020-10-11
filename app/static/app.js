ee42fdd98f23efde9ea51d7ee4ba3090f8b3032d
elsoleiro <elsoleiro@gmail.com>
2020-10-11 14:46:41 +0100

    counter added

--- app/static/app.js
+++ app/static/app.js
@@ -1,13 +1,17 @@
-function getVar() {
-    document.getElementById("var").innerHTML = max;
-    
-};
-
-
+let max;
+const myForm = document.querySelector("#my-form-id")
+myForm.addEventListener("click", (e) => {
+  e.preventDefault()
+  const myInput = document.querySelector("#my-name-input-id")
+  const myInputValue = myInput.value
+  max = parseInt(myInput.value) + 1
+  const myOutput = document.querySelector(".output")
+  myOutput.textContent= myInputValue
+});
 var clicks = 0;
 function counter() {
     clicks += 1;
-    if (clicks == 10) {
+    if (clicks == max) {
         clicks = 0
     };
     document.getElementById("clicks").innerHTML = clicks;
