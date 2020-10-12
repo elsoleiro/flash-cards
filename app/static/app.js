e3b0fdc4564cce19f6f752d6095ac9983179b7bb
elsoleiro <elsoleiro@gmail.com>
2020-10-12 17:27:03 +0100

    JS now flips card

--- app/static/app.js
+++ app/static/app.js
@@ -1,1 +1,12 @@
 console.log(cardFront, cardBack)
+document.getElementById("card").innerHTML = cardFront;
+
+
+function flipCard() {
+  var x = document.getElementById("card");
+  if (x.innerHTML === cardFront) {
+    x.innerHTML = cardBack;
+  } else {
+    x.innerHTML = cardFront;
+  }
+} 
