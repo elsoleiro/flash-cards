3acff7b29bd6fad11316022d6ea5bf8367b92050
elsoleiro <elsoleiro@gmail.com>
2020-10-16 11:57:26 +0100

    flip card reintroduced

--- app/static/app.js
+++ app/static/app.js
@@ -1,9 +1,23 @@
-console.log(x);
-
-console.log
+var objects = [];
+x.forEach(loopObjects);
+function loopObjects(item, index) {
+    objects.push(JSON.parse(item))
+};
 
 
 // technically speaking, we can use js to call the backend
 // we could also, since we have the cards, push the cards
 
+
+
+var j = 0;
+document.getElementById("card").innerHTML = objects[j].front;
+function flipCard() {
+  var x = document.getElementById("card");
+  if (x.innerHTML === objects[j].front) {
+    x.innerHTML = objects[j].back;
+  } else {
+    x.innerHTML = objects[j].front;
+  }
+}
 
