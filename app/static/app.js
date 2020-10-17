8b1444bf02eef54332da21298b34be852c1a8d96
elsoleiro <elsoleiro@gmail.com>
2020-10-17 12:02:39 +0100

    working on next card button

--- app/static/app.js
+++ app/static/app.js
@@ -5,10 +5,25 @@ function loopObjects(item, index) {
 };
 
 
+console.log(x)
+
+
+console.log(objects)
 // technically speaking, we can use js to call the backend
 // we could also, since we have the cards, push the cards
 
+// look into pagination, there may be a lot of load on the db depending
+// on the traffic, one option is to learn how to serve x at a time
+//
+// look into design patterns, search solid principles, book called
+// clean architecture, good high level overview of an application
 
+// look into cacheing
+//
+// use ajax request to whatever URL
+// if you pass data you use post
+// ajax is async java pretty much, js has a method
+// called fetch
 
 var j = 0;
 document.getElementById("card").innerHTML = objects[j].front;
@@ -19,5 +34,6 @@ function flipCard() {
   } else {
     x.innerHTML = objects[j].front;
   }
-}
+};
+
 
