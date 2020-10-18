7fb353a58d33c0f4d310b9b044f52e6fe8cbd27f
elsoleiro <elsoleiro@gmail.com>
2020-10-18 16:01:47 +0100

    look for another implementation of this

--- app/static/apptest.js
+++ app/static/apptest.js
@@ -5,10 +5,12 @@ function loopObjects(item, index) {
 };
 
 document.getElementById('cardFront').innerHTML = objects[0].front
+document.getElementById('cardBack').innerHTML = objects[0].back
 
 document.addEventListener('DOMContentLoaded', function(event) {
     document.getElementById('flip-card-btn-turn-to-back').style.visibility = 'visible';
     document.getElementById('flip-card-btn-turn-to-front').style.visibility = 'visible';
+    document.getElementById('next-card-btn').style.visibility = 'visible';
     document.getElementById('flip-card-btn-turn-to-back').onclick = function() {
         document.getElementById('flip-card').classList.toggle('do-flip');
     };
