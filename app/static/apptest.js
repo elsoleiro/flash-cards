07da90316d8e71c43ec8bfa2af206ea8efc70da1
elsoleiro <elsoleiro@gmail.com>
2020-10-18 18:40:13 +0100

    almost there, front is not hidden on flip

--- app/static/apptest.js
+++ app/static/apptest.js
@@ -4,17 +4,8 @@ function loopObjects(item, index) {
     objects.push(JSON.parse(item))
 };
 
-document.getElementById('cardFront').innerHTML = objects[0].front
-document.getElementById('cardBack').innerHTML = objects[0].back
+const card = document.querySelector('.card-inner')
 
-document.addEventListener('DOMContentLoaded', function(event) {
-    document.getElementById('flip-card-btn-turn-to-back').style.visibility = 'visible';
-    document.getElementById('flip-card-btn-turn-to-front').style.visibility = 'visible';
-    document.getElementById('next-card-btn').style.visibility = 'visible';
-    document.getElementById('flip-card-btn-turn-to-back').onclick = function() {
-        document.getElementById('flip-card').classList.toggle('do-flip');
-    };
-    document.getElementById('flip-card-btn-turn-to-front').onclick = function() {
-        document.getElementById('flip-card').classList.toggle('do-flip');
-    };
+card.addEventListener('click', function() {
+    card.classList.toggle('is-flipped')
 });
