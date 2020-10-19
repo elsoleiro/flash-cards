c857970fb01217110677f144784cdad92c5d5d78
elsoleiro <elsoleiro@gmail.com>
2020-10-19 18:47:25 +0100

    next card added

--- app/static/apptest.js
+++ app/static/apptest.js
@@ -9,3 +9,16 @@ const card = document.querySelector('.card__inner')
 card.addEventListener('click', function() {
     card.classList.toggle('is-flipped')
 });
+var j = 0;
+var cardFront = document.querySelector('.cardFront').innerHTML = objects[j].front
+var cardBack = document.querySelector('.cardBack').innerHTML = objects[j].back
+
+function nextCard() {
+    j += 1;
+    var k = j % objects.length;
+    document.querySelector('.cardFront').innerHTML = objects[k].front;
+    document.querySelector('.cardBack').innerHTML = objects[k].back;
+    
+};
+
+
