e3f9d0f9ae32d19430948d0863024c6e26bd38e7
elsoleiro <elsoleiro@gmail.com>
2020-10-19 18:33:08 +0100

    border removed in css

--- app/static/apptest.js
+++ app/static/apptest.js
@@ -4,7 +4,7 @@ function loopObjects(item, index) {
     objects.push(JSON.parse(item))
 };
 
-const card = document.querySelector('.card-inner')
+const card = document.querySelector('.card__inner')
 
 card.addEventListener('click', function() {
     card.classList.toggle('is-flipped')
